1135abc5856acbead52c49aaa070c125a37bcf39
Will 保哥 <doggy.huang@gmail.com>
2022-01-18 11:45:25 +0800

    示範子路由語法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'utilities',
+    children: [
+      { path: 'colors', component: ColorsComponent },
+    ]
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
