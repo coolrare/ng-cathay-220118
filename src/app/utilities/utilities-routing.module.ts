22114d253ee013122002dfb66aa2c9f38856a717
Will 保哥 <doggy.huang@gmail.com>
2022-01-18 12:08:43 +0800

    示範在延遲載入的子路由模組加入元件與路由

--- src/app/utilities/utilities-routing.module.ts
+++ src/app/utilities/utilities-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColorsComponent } from './colors/colors.component';
 
 const routes: Routes = [
-
+  { path: 'colors', component: ColorsComponent },
 ];
 
 @NgModule({
