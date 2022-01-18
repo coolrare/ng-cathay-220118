312acf5df80243807f80dff19bbb429c07ca5f6d
Will 保哥 <doggy.huang@gmail.com>
2022-01-18 13:42:27 +0800

    示範 preloadingStrategy: PreloadAllModules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -17,7 +17,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
