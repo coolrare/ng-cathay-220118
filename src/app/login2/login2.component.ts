94243eee6bf29a2be9687b129e0d13565d26cee7
Will 保哥 <doggy.huang@gmail.com>
2022-01-20 13:50:40 +0800

    練習替 Reactive Forms 加入驗證器並調整頁面顯示邏輯

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login2.component.html',
@@ -24,16 +24,46 @@ export class Login2Component implements OnInit, OnDestroy {
     this.orig_body_class = document.body.className;
     document.body.className = 'bg-gradient-primary';
 
+    // this.form = this.fb.group({
+    //   email: ['user@example.com',
+    //     [
+    //       Validators.required,
+    //       Validators.email,
+    //       Validators.minLength(5),
+    //     ]
+    //   ],
+    //   password: '123123',
+    //   isRemember: true,
+    // });
+
     this.form = this.fb.group({
-      email: 'user@example.com',
-      password: '123123',
+      email: this.fb.control('user@example.com', [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(5),
+      ]),
+
+      password: this.fb.control('123123', [
+        Validators.required
+      ]),
+
       isRemember: true,
-    })
+    });
 
   }
 
   ngOnDestroy(): void {
     document.body.className = this.orig_body_class;
+  }
+
+  doSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted');
+    }
+  }
+
+  isValidateForm(controlName: string) {
+    return (this.form.get(controlName)?.touched);
   }
 
 }
